e0559079490e1e28324718163d7f32c9f85bdefb
Anderson Barbosa dos Santos <dev.anderson.santos@gmail.com>
2018-06-29 13:10:04 -0300

    Create pessoas.js

---
+++ projeto/html/js/pessoas.js
@@ -0,0 +1,18 @@
+[
+  {
+    nome: "Anderson",
+    email: anderson@email.com
+  },
+  {
+		nome: 'André',
+		email: 'andre@email.com'
+	},
+	{
+		nome: 'Joao',
+		email: 'joao@email.com'
+	},  
+	{
+		nome: 'Maria',
+		email: 'maria@email.com'
+	}
+];
